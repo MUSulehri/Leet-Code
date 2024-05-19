83a900fd1071f0b9ce48365d038068e0df2f0bb9
Muhammad Usman Sulehri <150555923+MUSulehri@users.noreply.github.com>
2024-05-20 01:03:42 +0500

    Time: 1 ms (92.25%), Space: 45.1 MB (96.36%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -3,6 +3,7 @@ class Solution {
         int direct_profit = 0;
         int index = 1;
         int minIndex = 0;
+        int profit = 0;
         while (index < prices.length)
         {
             if (direct_profit < (prices[index] - prices[minIndex]))
@@ -13,12 +14,6 @@ class Solution {
             {
                 minIndex = index;
             }
-            index++;
-        }
-        index = 1;
-        int profit = 0;
-        while (index < prices.length)
-        {
             if (prices[index] - prices[index - 1] > 0)
             {
                 profit += prices[index] - prices[index - 1];
