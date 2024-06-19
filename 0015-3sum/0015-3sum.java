b66c91f2733558573527b0efcb8a87f46fd77ac9
Muhammad Usman Sulehri <150555923+MUSulehri@users.noreply.github.com>
2024-06-20 02:44:46 +0500

    Time: 168 ms (32.83%), Space: 53.7 MB (6.40%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,54 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-           List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, Integer> pos = new HashMap<>();
+        Map<Integer, Integer> neg = new HashMap<>();
+        int zeroCount = 0;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int target = -nums[i];
-                int left = i + 1, right = nums.length - 1;
+        // Populate the maps
+        for (int num : nums) {
+            if (num > 0) {
+                pos.put(num, pos.getOrDefault(num, 0) + 1);
+            } else if (num < 0) {
+                neg.put(num, neg.getOrDefault(num, 0) + 1);
+            } else {
+                zeroCount++;
+            }
+        }
 
-                while (left < right) {
-                    if (nums[left] + nums[right] == target) {
-                        result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-                        left++;
-                        right--;
-                    } else if (nums[left] + nums[right] < target) {
-                        left++;
-                    } else {
-                        right--;
+        // Check for the triplet (0, 0, 0)
+        if (zeroCount >= 3) {
+            result.add(Arrays.asList(0, 0, 0));
+        }
+
+        Set<List<Integer>> resultSet = new HashSet<>();
+        List<Integer> triplet;
+        // Check for other combinations
+        for (Map.Entry<Integer, Integer> elePos : pos.entrySet()) {
+            int posVal = elePos.getKey();
+            for (Map.Entry<Integer, Integer> eleNeg : neg.entrySet()) {
+                int negVal = eleNeg.getKey();
+                int rem = -(posVal + negVal);
+
+                if (rem == 0 && zeroCount > 0) {
+                    resultSet.add(Arrays.asList(posVal, negVal, 0));
+                } else if (rem > 0 && pos.containsKey(rem)) {
+                    if ((rem == posVal && pos.get(rem) > 1) || rem != posVal) {
+                        triplet = Arrays.asList(posVal, negVal, rem);
+                        Collections.sort(triplet);
+                        resultSet.add(triplet);
+                    }
+                } else if (rem < 0 && neg.containsKey(rem)) {
+                    if ((rem == negVal && neg.get(rem) > 1) || rem != negVal) {
+                        triplet = Arrays.asList(posVal, negVal, rem);
+                        Collections.sort(triplet);
+                        resultSet.add(triplet);
                     }
                 }
             }
         }
-
+        
+        result.addAll(resultSet);
         return result;
     }
-
 }
